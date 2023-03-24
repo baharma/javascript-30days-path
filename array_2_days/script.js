9975410f547880c871db0005036b96ad8287da34
baharma <bagusaditya1899@gmail.com>
2023-03-24 23:19:40 +0800

    higher

--- array_2_days/script.js
+++ array_2_days/script.js
@@ -1,66 +1,66 @@
-// const arr = []
-// console.log(arr)
-// play = array(0)
+const arr = []
+console.log(arr)
+play = array(0)
 
-// const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
-// const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
-// const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
-// const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
-// const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
-// const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
 
-// console.log('Numbers:', numbers)
-// console.log('Number of numbers:', numbers.length)
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
 
-// console.log('Fruits:', fruits)
-// console.log('Number of fruits:', fruits.length)
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
 
-// console.log('Vegetables:', vegetables)
-// console.log('Number of vegetables:', vegetables.length)
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
 
-// console.log('Animal products:', animalProducts)
-// console.log('Number of animal products:', animalProducts.length)
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
 
-// console.log('Web technologies:', webTechs)
-// console.log('Number of web technologies:', webTechs.length)
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
 
-// console.log('Countries:', countries)
-// console.log('Number of countries:', countries.length)
+console.log('Countries:', countries)
+console.log('Number of countries:', countries.length)
 
-// menghitung julah array pada variabel di buat
-
-
+menghitung julah array pada variabel di buat
 
 
-// 
-// const numbers  = [1,2,3,4]
-// numbers[0] = 10
-// numbers[1] = 20
-
-// console.log(numbers)
 
 
-// const countries = [
-//     'Albania',
-//     'Bolivia',
-//     'Canada',
-//     'Denmark',
-//     'Ethiopia',
-//     'Finland',
-//     'Germany',
-//     'Hungary',
-//     'Ireland',
-//     'Japan',
-//     'Kenya'
-//   ]
 
-// countries[0] = "Indonesia"
-// let lesIndex = countries.length - 1
-// countries[lesIndex] = "Nigeria"
+const numbers  = [1,2,3,4]
+numbers[0] = 10
+numbers[1] = 20
 
-// console.log(countries)
+console.log(numbers)
 
-// the input in array the last and start 
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+
+countries[0] = "Indonesia"
+let lesIndex = countries.length - 1
+countries[lesIndex] = "Nigeria"
+
+console.log(countries)
+
+the input in array the last and start 
 
 
 
